00ef22ef242367298dc9d79e2ff1615232791c5c
Jack Mehmet <jmm546@york.ac.uk>
2020-07-25 10:24:06 +0100

    completed csv

--- 2020-07-20 csv/fix_csv.py
+++ 2020-07-20 csv/fix_csv.py
@@ -12,36 +12,17 @@ args = parser.parse_args()
 delimiter_orig = args.delimiter_orig
 quote = args.quote
 
-delimiter_new = ","
-
 with open(args.filename_orig, "r", newline="") as orig_f:
-    if None in [delimiter_orig, quote]:
-        delim_count = {}
-        quote_count = {}
-
-        for line in orig_f:
-            for c in set(line):
-                if c in ["\"", "'"]:
-                    quote_count[c] = quote_count.get(c, 0) + 1
-                elif c in string.punctuation:
-                    delim_count[c] = delim_count.get(c, 0) + 1
-        
-        if delimiter_orig is None:
-            delim_count_sorted = sorted(delim_count.items(), key=lambda t: t[1], reverse=True)
-            delimiter_orig = delim_count_sorted[0][0]
-            
-        if quote is None:
-            if quote_count:
-                quote_count_sorted = sorted(quote_count.items(), key=lambda t: t[1], reverse=True)
-                quote = quote_count_sorted[0][0]
-            else:
-                quote = "\""
-
+    if delimiter_orig or quote:
+        reader_kwargs = {k: v 
+            for k, v 
+            in zip(["delimiter", "quotechar"], [delimiter_orig, quote])
+            if v}
+    else:
+        reader_kwargs = {"dialect": csv.Sniffer().sniff(orig_f.read())}
         orig_f.seek(0)
-
-    reader = csv.reader(orig_f, delimiter=delimiter_orig, quotechar=quote)
+    
+    reader = csv.reader(orig_f, **reader_kwargs)
 
     with open(args.filename_new, "w", newline="") as new_f:
-        writer = csv.writer(new_f, delimiter=delimiter_new, quotechar=quote, quoting=csv.QUOTE_MINIMAL)
-
-        writer.writerows(reader)
+        csv.writer(new_f).writerows(reader)
