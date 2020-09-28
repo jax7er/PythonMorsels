34827cd097f43504fd8b51a9e6debe62ebac57bb
Jack Mehmet <Jack.Mehmet@dyson.com>
2020-09-28 21:28:16 +0100

    updated git_helper

--- 2020-09-28 deep_add/deep_add.py
+++ 2020-09-28 deep_add/deep_add.py
@@ -2,21 +2,22 @@
 def deep_add(arg, start = None):
     total = start
 
-    try:
+    try: # check iterable
         list_arg = list(arg)
     except TypeError: # not iterable
-        if arg is None:
+        if arg is None: # check None
             raise
-        else:
+        else: # summable type
             total = total + value if total else value
-    else:
+    else: # iterable
         for value in list_arg:
-            try:
+            try: # check iterable
                 list_value = list(value)
             except TypeError: # not iterable
                 total = total + value if total else value
-            else:
-                if list_value:
+            else: # iterable
+                if list_value: # check not empty
+                    # recursion
                     value = deep_add(list_value, 0)
 
                     total = total + value if total else value
